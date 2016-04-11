40354a2662e5c851930970b74558c48572d9f59c
Vladimir Lozitskiy <loza-w@mail.ru>
2016-04-11 09:59:32 +0300

    Update index view

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 #encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+
 
 set :database, "sqlite3:barbershop.db"
 
@@ -13,7 +15,9 @@ end
 class Barber < ActiveRecord::Base
 
 end
-	
+
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@barbers = Barber.all
+	 erb :index
+
 end
