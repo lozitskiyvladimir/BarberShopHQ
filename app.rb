db622a7ff2cf40b4d05dc52d5c0cea5aa3ec0127
Vladimir Lozitskiy <loza-w@mail.ru>
2016-04-22 08:30:29 +0300

    Show client information on client page

--- app.rb
+++ app.rb
@@ -68,3 +68,13 @@ get '/barber/:id' do
 	@barber = Barber.find(params[:id])
 	erb :barber
 end
+
+get '/bookings' do
+	@clients = Client.order('created_at DESC')
+		erb :bookings
+end
+
+get '/client/:id' do
+	@client = Client.find params[:id]
+	erb :clients
+end
